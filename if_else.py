04bc5e4e77a4f6bab980ee632d2b3407249a3ebd
singcl <24661881@qq.com>
2018-07-24 20:08:26 +0800

    :art: python 之 if_elif_else 流程控制

---
+++ if_else.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*- 或者 #coding=utf-8 或者#coding:utf 都行
+# coding:utf-8
+
+""""if else 流程控制"""
+
+x = int(raw_input(u"请输入一个整数"))
+if x < 0:
+    x = 0
+    print('Negative changed to zero')
+elif x == 0:
+    print('Zero')
+elif x == 1:
+    print('Single')
+else:
+    print('More')
