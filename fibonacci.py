7bea4d25a92be9c5517acfc74df36a7210415d08
singcl <24661881@qq.com>
2018-07-24 13:44:31 +0800

    :art: python 之 斐波那契计算

---
+++ fibonacci.py
@@ -0,0 +1,9 @@
+""""
+Fibonacci series:
+he sum of two elements defines the next
+"""
+a, b = 0, 1
+
+while b < 100:
+    print b
+    a, b = b, a + b
--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
-# -*- coding: UTF-8 -*- 或者 #coding=utf-8 都行
+# -*- coding: UTF-8 -*- 或者 #coding=utf-8 或者#coding:utf 都行
 # -*- coding: utf-8 -*-
 ''' hello world '''
 
 NAME = raw_input('please enter your name: ')
-print 'hello,世界', NAME
+print "hello," u'世界', NAME
