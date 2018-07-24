6050b54a45ca338af7b8d1c828a1e547717b0f35
singcl <24661881@qq.com>
2018-07-24 13:18:53 +0800

    :art: pylint 类似eslint 的编码规范

--- hello.py
+++ hello.py
@@ -1,2 +1,6 @@
-name = raw_input('please enter your name: ')
-print 'hello,', name
+# -*- coding: UTF-8 -*- 或者 #coding=utf-8 都行
+# -*- coding: utf-8 -*-
+''' hello world '''
+
+NAME = raw_input('please enter your name: ')
+print 'hello,世界', NAME
