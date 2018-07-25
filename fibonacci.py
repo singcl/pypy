54c2bde78902f2dea3bce706f39cf4ceeb5a81fc
singcl <24661881@qq.com>
2018-07-26 01:18:55 +0800

    :art: fibonacci函数 Python函数定义

--- fibonacci.py
+++ fibonacci.py
@@ -7,3 +7,18 @@ a, b = 0, 1
 while b < 100:
     print b
     a, b = b, a + b
+
+
+# No shadow variables
+def fib2(n):
+    """Return a list containing the Fibonacci series up to n."""
+    result = []
+    x, y = 0, 1
+    while x < n:
+        result.append(x)
+        x, y = y, x + y
+    return result
+
+
+f100 = fib2(100)
+print f100
