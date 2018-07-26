bf9fccc6a59d373c940dfc912f6e8c72cb316cf1
singcl <24661881@qq.com>
2018-07-26 10:43:15 +0800

    :art: Python 函数默认值在函数作用域解析，默认值只赋值一次

--- def_default_parameter.py
+++ def_default_parameter.py
@@ -17,3 +17,29 @@ def ask_ok(prompt, reties=4, complaint="Yes or No, Please"):
 
 
 ask_ok('Do you really want to quit?')
+
+
+# 默认值在函数的作用域内解析
+i = 5
+
+
+def foo(arg=i):
+    """默认值在函数的作用域内解析"""
+    print arg
+
+
+i = 6
+foo()
+
+# 重要警告: 默认值只被赋值一次。这使得当默认值是可变对象时会有所不同
+
+
+def bar(a, list=[]):
+    """重要警告: 默认值只被赋值一次。这使得当默认值是可变对象时会有所不同"""
+    list.append(a)
+    return list
+
+
+print bar(1)
+print bar(2)
+print bar(3)
