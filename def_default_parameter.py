b529e6072dc927262ce474a2440d71cd236a0ef2
singcl <24661881@qq.com>
2018-07-26 13:03:46 +0800

    :art: Python 关键字参数之*args and **kwargs

--- def_default_parameter.py
+++ def_default_parameter.py
@@ -43,3 +43,38 @@ def bar(a, list=[]):
 print bar(1)
 print bar(2)
 print bar(3)
+
+
+# 关键字函数
+def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
+    """关键字参数的基本使用规则"""
+    print "-- This parrot wouldn't", action,
+    print "if you put", voltage, "volts through it."
+    print "-- Lovely plumage, the", type
+    print "-- It's", state, "!"
+
+
+parrot(1000)                                          # 1 positional argument
+parrot(voltage=1000)                                  # 1 keyword argument
+parrot(voltage=1000000, action='VOOOOOM')             # 2 keyword arguments
+parrot(action='VOOOOOM', voltage=1000000)             # 2 keyword arguments
+parrot('a million', 'bereft of life', 'jump')         # 3 positional arguments
+parrot('a thousand', state='pushing up the daisies')  # 1 positional, 1 keyword
+
+
+# 未出现在形式参数列表中的关键字参数
+def cheeseshop(kind, *args, **kwargs):
+    """未出现在形式参数列表中的关键字参数"""
+    print '-' * 40
+    print "-- Do you have any", kind, "?"
+    print "-- I'm sorry, we're all out of", kind
+    for arg in args:
+        print arg
+    print '-' * 40
+    keys = sorted(kwargs.keys())
+    for kw in keys:
+        print kw, ":", kwargs[kw]
+
+
+cheeseshop("Limburger", "It's very runny, sir.", "It's really very, VERY runny, sir.", shopkeeper='Michael Palin',
+           client="John Cleese", sketch="Cheese Shop Sketch")
