0d89484cc11182e95e4d130ee07d2764d15ff533
singcl <24661881@qq.com>
2018-08-07 01:06:18 +0800

    :art: Python Flask 装饰器简单使用

---
+++ my_flask_app/app.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+"""Python装饰器在flask中的简单应用
+"""
+from flask import render_template
+from flask.ext.login import login_required, current_user
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route(/dashboard)
+@login_required
+def account():
+    return render_template("account.html")
+
