d61fde7366415d83130f23caf6232645ee14b930
singcl <24661881@qq.com>
2018-08-07 00:52:59 +0800

    :art: Python Flask应用如何处理配置 - shell读取环境变量来配置

--- my_flask_app/myapp/__init__.py
+++ my_flask_app/myapp/__init__.py
@@ -7,7 +7,18 @@
 from flask import Flask
 
 app = Flask(__name__, instance_relative_config=True)
-app.config.from_object('config')
+
+# app.config.from_object('config')
+# app.config.from_pyfile('config.py')
+
+# Load the default configuration
+app.config.from_object('config.default')
+
+# Load the configuration from the instance folder
 app.config.from_pyfile('config.py')
+
+# Load the file specified by the APP_CONFIG_FILE environment variable
+# Variables defined here will override those in the default configuration
+app.config.from_envvar('APP_CONFIG_FILE')
 
 # Now we can access the configuration variables via app.config["VAR_NAME"].
---
+++ my_flask_app/start.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# start.sh
+
+APP_CONFIG_FILE=/var/www/yourapp/config/production.py
+python run.py
