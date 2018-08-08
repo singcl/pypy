864441073b2426c767b71474139515f74afeb317
singcl <24661881@qq.com>
2018-08-08 10:33:58 +0800

    :art: Python 获取win10没事壁纸

---
+++ win_paper.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+"""Python获取win10每日壁纸
+"""
+
+import os
+import shutil
+from datetime import datetime
+
+save_folder = dir_path = os.path.dirname(os.path.realpath(__file__)) + "\wallpapers"
+wallpaper_folder = os.getenv('LOCALAPPDATA') + ('Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy' +
+                                                'LocalState\Assets')
+wallpapers = os.listdir(wallpaper_folder)
+for wallpaper in wallpapers:
+    wallpaper_path = os.path.join(wallpaper_folder, wallpaper)
+    if os.path.getsize(wallpaper_path) / 1024 < 150:
+        continue
+    wallpaper_name = wallpaper + '.jpg'
+    save_path = os.path.join(save_folder, wallpaper_name)
+    shutil.copyfile(wallpaper_path, save_path)
+    print 'Save wallpaper', save_path
