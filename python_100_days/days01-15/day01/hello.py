fe96807035eb62bdffe8452d706507e7c1f646fd
singcl <24661881@qq.com>
2018-11-25 00:43:33 +0800

    :art: print()的基本用法 in Pyton3x

---
+++ python_100_days/days01-15/day01/hello.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+"""
+第一个Python程序
+问候世界
+
+Version: 0.1
+Author: singcl
+Date: 2018-11-25
+
+请将该文件命名为hello.py并在终端中通过下面的命令运行它
+python hello.py
+"""
+
+print("hello world!")
+print("您好", "中国")
+print('hello', 'world', sep=',', end='!')
+print('goodbye', 'world', end='!\n')
+
+
