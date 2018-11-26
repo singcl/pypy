017ed72d2e6678cc10a77fd56172ef885d98c56c
singcl <24661881@qq.com>
2018-11-26 10:01:04 +0800

    :art: type() return the type of variables

---
+++ python_100_days/1.2_variable4.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+检查变量的类型
+
+Version: 0.1
+Author: Singcl
+Date: 2018-11-26
+
+"""
+
+a = 100
+b = 10000000000000000000000000
+c = 12.3222
+d = 1 + 5j
+e = 'A'
+f = "hello world"
+g = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
