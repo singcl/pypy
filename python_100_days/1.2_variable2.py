9dab17094c9c4d1a8e0a8f27838d0d53faec734e
singcl <24661881@qq.com>
2018-11-26 09:44:12 +0800

    :art: input() and int() in Py3

---
+++ python_100_days/1.2_variable2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#-*- codeing: utf-8 -*-
+
+"""
+将input函数输入的数据保存在变量中并进行操作
+
+Version: 0.1
+Author: Singcl
+Date: 2018-11-26
+"""
+
+a = int(input("a = "))
+b = int(input("b = "))
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
