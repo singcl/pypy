dd5caa8f982fe687d7c90f5baa0ac7a8ba490dcc
singcl <24661881@qq.com>
2018-11-27 22:18:14 +0800

    :art: 分段函数求值

---
+++ python_100_days/1.3_piecewise.py
@@ -0,0 +1,26 @@
+#!usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+
+分段函数求值
+
+        3x - 5	(x > 1)
+f(x) =	x + 2	(-1 <= x <= 1)
+		5x + 3	(x < -1
+
+Version: 0.1
+Author: singcl
+Date: 2018-11-27
+
+"""
+
+x = float(input("x = "))
+if x > 1:
+    y = 3 * x - 5
+elif x >= -1:
+    y = x + 2
+else:
+    y = 5 * x + 3
+
+print("f(%.2f) = %.2f" % (x, y))
