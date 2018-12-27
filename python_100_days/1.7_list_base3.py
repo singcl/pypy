b79d95d3618d8377d2bf8e42c8cbaebd5190f503
singcl <24661881@qq.com>
2018-12-28 01:25:01 +0800

    :art: 列表基本操作 - 多种方法生成列表

---
+++ python_100_days/1.7_list_base3.py
@@ -0,0 +1,54 @@
+#!usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+
+生成列表
+	- 用range创建数字列表
+	- 生成表达式
+	- 生成器
+
+Version: 0.1
+Author: singcl
+Date: 2018-12-28
+
+"""
+
+
+def fib(n):
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+        yield a
+
+
+def main():
+    # 用range创建数值列表
+    list1 = list(range(0, 1))
+    print(list1)
+
+    # 生成表达式
+    list2 = [x * x for x in range(1, 11)]
+    print(list2)
+
+    list3 = [m + n for m in 'ABCDEFG' for n in '12345']
+    print(list3)
+    print(len(list3))
+
+    # 生成器(节省空间但生成下一个元素时需要花费时间)
+    gen = (m + n for m in 'ABCDEFG' for n in '12345')
+    print(gen)
+    for elem in gen:
+        print(elem, end=" ")
+    print()
+
+    gen = fib(20)
+    print(gen)
+
+    for elem in gen:
+        print(elem, end=' ')
+    print()
+
+
+if __name__ == "__main__":
+    main()
