7a79318ddf7d9d54d04401bbd58d98de5ce3557b
singcl <24661881@qq.com>
2019-01-10 23:26:17 +0800

    :art: 两种方法定义类的属性

--- python_100_days/1.9_car1.py
+++ python_100_days/1.9_car1.py
@@ -16,12 +16,14 @@ Date: 2019-01-10
 class Car(object):
 
     # 使用__slots__属性限制对象能够绑定的属性
-    __slots__ = ('_brand', '_max_speed')
+    __slots__ = ('_brand', '_max_speed', '_color')
 
-    def __init__(self, brand, max_speed):
+    def __init__(self, brand, max_speed, color):
         self._brand = brand
         self._max_speed = max_speed
+        self._color = color
 
+    # 第一种方法（装饰器）定义类的属性
     @property
     def brand(self):
         return self._brand
@@ -44,11 +46,23 @@ class Car(object):
             raise ValueError('Invalid max speed for car')
         self._max_speed = max_speed
 
+    # 第二种方法定义类的属性
+    def get_color(self):
+        return self._color
+
+    def set_color(self, value):
+        self._color = value
+
+    def del_color(self):
+        del self._color
+
+    color = property(get_color, set_color, del_color, '')
+
     def __str__(self):
         return 'Car: [品牌=%s, 最高时速=%d]' % (self._brand, self._max_speed)
 
 
-car = Car('qq', 120)
+car = Car('qq', 120, 'red')
 print(car)
 # ValueError
 # car.max_speed = -100
@@ -61,9 +75,9 @@ print(car)
 # del car.brand
 # 属性的实现
 print(Car.brand)
-print(Car.brand.fget)
-print(Car.brand.fset)
-print(Car.brand.fdel)
+print(Car.color.fget)
+print(Car.color.fset)
+print(Car.color.fdel)
 
 # 通过上面的代码帮助学生理解之前提到的包装器的概念
 # Python中有很多类似的语法糖后面还会出现这样的东西
