4a9814b35e2adc022784a6cf7812df34d740d034
singcl <24661881@qq.com>
2019-01-25 00:02:56 +0800

    :art: Python GUI

---
+++ python_100_days/1.10_gui1.py
@@ -0,0 +1,21 @@
+"""
+使用tkinter创建GUI
+	- 顶层窗口
+	- 控件
+	- 布局
+	- 事件回调
+Version: 0.1
+Author: singcl
+Date: 2019-01-25
+"""
+
+import tkinter
+import tkinter.messagebox
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
