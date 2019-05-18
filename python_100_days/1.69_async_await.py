66b9e3ba5d0f129d925779e94d699066561849bb
singcl <24661881@qq.com>
2019-05-18 19:31:38 +0800

    :art: 异步上下文管理器”async with”

---
+++ python_100_days/1.69_async_await.py
@@ -0,0 +1,42 @@
+#!usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Python 并发下载
+异步I/O - 非阻塞式I/O操作
+
+Version: 0.1
+Author: singcl
+Date: 2019-5-18
+"""
+
+import asyncio
+import aiohttp
+
+
+async def download(url):
+    print('Fetch:', url)
+    # 异步上下文管理器”async with”
+    # @see https://blog.csdn.net/tinyzhao/article/details/52684473
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            print(url, '--->', resp.status)
+            print(url, '--->', resp.cookies)
+            print('\n\n', await resp.text())
+
+
+def main():
+    loop = asyncio.get_event_loop()
+    urls = [
+        'https://www.baidu.com',
+        'http://www.sohu.com/',
+        'http://www.sina.com.cn/',
+        'https://www.taobao.com/',
+        'https://www.jd.com/'
+    ]
+    corutines = [download(url) for url in urls]
+    loop.run_until_complete(asyncio.wait(corutines))
+    loop.close()
+
+
+if __name__ == '__main__':
+    main()
