a4ee21c6bc12696aacf643480b484d0b78bbd222
singcl <1276837689@qq.com>
2020-04-05 18:23:35 +0800

    flask app

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -1,16 +1,48 @@
 # coding: utf-8
 
-"""Python装饰器在flask中的简单应用
-"""
-from flask import render_template
-from flask.ext.login import login_required, current_user
+from flask import Flask, escape, url_for
+
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    # url_for 构造login url
+    login_url = url_for("login")
+    return f"<a href=\"{login_url}\">登陆</a>"
 
-@app.route("/dashboard")
-@login_required
-def account():
-    return render_template("account.html")
+@app.route("/login")
+def login():
+    profile_url = url_for('show_user_profile', username='singcl')
+    return f"""
+    <div style='text-align: center'>
+        <p>欢迎来到登陆页面</p>
+        <a href="{profile_url}">个人简介</a>
+    </div>
+    """
 
+@app.route('/hello')
+def hello():
+    return "hello world"
+
+@app.route("/user/<username>")
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % (escape(username))
+
+@app.route("/post/<int:post_id>")
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route("/path/<path:subpath>")
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % escape(subpath)
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
