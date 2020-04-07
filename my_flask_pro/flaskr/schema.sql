06d793028369df6491a2e2680b2e8af2ad316ff0
singcl <1276837689@qq.com>
2020-04-07 13:57:43 +0800

    初始化数据库

--- my_flask_pro/flaskr/schema.sql
+++ my_flask_pro/flaskr/schema.sql
@@ -6,3 +6,12 @@ CREATE TABLE user (
     username TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL
 );
+
+CREATE TABLE post(
+    id INTEGER PRIMARY KEY AUTOINCEMENT,
+    author_id INTEGER NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES user (id)
+);
