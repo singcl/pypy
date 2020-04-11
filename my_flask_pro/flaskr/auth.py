c14e68673c9d2239acf6a67227975a023509b80a
singcl <1276837689@qq.com>
2020-04-11 22:52:55 +0800

    请求前读取存储user；登陆验证装饰器

--- my_flask_pro/flaskr/auth.py
+++ my_flask_pro/flaskr/auth.py
@@ -61,7 +61,26 @@ def login():
 
     return render_template('auth/login.html')
 
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute('SELECT * FROM user WHERE id = ?', (user_id,)).fetchone()
+
 @bp.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('index'))
+
+# 在其他视图中验证
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        else:
+            return view(**kwargs)
+    return wrapped_view
